80bbfed3ab9641d296d6f06e4abb1216e604b2ad
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-04-30 19:21:20 -0700

    Import node files

--- djikstra.py
+++ djikstra.py
@@ -1,4 +1,12 @@
+import os
+from node import Node, Edge
+script_dir = os.path.dirname(__file__)
+rel_path = "djikstraData.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
 
-
-def djikstra(graph, start_node, end_node):
+def convert_adj_list_to_graph(file):
     
+
+
+
+# def djikstra(graph, start_node, end_node):
